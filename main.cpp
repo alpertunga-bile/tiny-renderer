1f92b0ebcf35008c7a3ea5a5869417087daa4a2b
alpertunga-bile <kingdom19online@hotmail.com>
2023-04-17 15:36:54 +0300

    Wireframe rendering is completed

--- main.cpp
+++ main.cpp
@@ -1,17 +1,40 @@
 #include "lineDrawing.hpp"
+#include "model.h"
 
 int main()
 {
-    const char* outputFile = "../output/output_line.tga";
+    const char* modelFile = "../models/african_head.obj";
+    const char* outputFile = "../output/output_wireframe.tga";
     const TGAColor white = TGAColor(255, 255, 255, 255);
-    const TGAColor red = TGAColor(255, 0, 0, 255);
+    
+    Model* model = new Model(modelFile);
 
-    TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white); 
-    line(20, 13, 40, 80, image, red); 
-    line(80, 40, 13, 20, image, red);
+    const int width = 800;
+    const int height = 800;
+
+    TGAImage image(width, height, TGAImage::RGB);
+    
+    for(int i = 0; i < model->nfaces(); i++)
+    {
+        std::vector<int> face = model->face(i);
+
+        for(int j = 0; j < 3; j++)
+        {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+
+            int x0 = (v0.x + 1.0f) * static_cast<float>(width) / 2.0f;
+            int y0 = (v0.y + 1.0f) * static_cast<float>(height) / 2.0f;
+            int x1 = (v1.x + 1.0f) * static_cast<float>(width) / 2.0f;
+            int y1 = (v1.y + 1.0f) * static_cast<float>(height) / 2.0f;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
+
     image.flip_vertically();
     image.write_tga_file(outputFile);
+
+    delete model;
     
     return 0;
 }
