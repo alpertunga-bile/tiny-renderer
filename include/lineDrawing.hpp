d3244723db980f166b8b80a345503ed3481bbf2d
alpertunga-bile <kingdom19online@hotmail.com>
2023-04-17 15:00:49 +0300

    Completed line rendering

--- include/lineDrawing.hpp
+++ include/lineDrawing.hpp
@@ -2,10 +2,48 @@
 
 inline void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
+    bool steep = false;
+    
+    // if steep transpose image
+    if(std::abs(x0 - x1) < std::abs(y0 - y1))
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+
+    // make always x1 & y1 > x0 & y0
+    if(x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    int dError = std::abs(dy) * 2;
+    int error = 0; // distance to best straight line
+    int y = y0;
+
     for(int x = x0; x <= x1; x++)
     {
-        float t = static_cast<float>(x - x0) / static_cast<float>(x1 - x0);
-        int y = y0 * (1.0f - t) + y1 * t;
-        image.set(x, y, color);
+        if(steep)
+        {
+            image.set(y, x, color); // detranspose
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+
+        error += dError;
+
+        // check if error is greater than one pixel
+        if(error > dx)
+        {
+            y += y1 > y0 ? 1 : -1;
+            error -= dx * 2;
+        }
     }   
 }
--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main()
     const TGAColor red = TGAColor(255, 0, 0, 255);
 
     TGAImage image(100, 100, TGAImage::RGB);
-    line(0, 0, 51, 42, image, white);
+    line(13, 20, 80, 40, image, white); 
+    line(20, 13, 40, 80, image, red); 
+    line(80, 40, 13, 20, image, red);
     image.flip_vertically();
     image.write_tga_file(outputFile);
     
